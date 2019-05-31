a69cd93d9c5c961a043701143a33b243da7dec55
Stephen Bannasch <stephen.bannasch@gmail.com>
2019-05-31 15:53:11 -0400

    extract Html game sections into separate functions

--- src/Main.elm
+++ src/Main.elm
@@ -40,120 +40,164 @@ view model =
     { title = "Elm 2048"
     , body =
         [ div [ class "container" ]
-            [ div [ class "heading" ]
-                [ h1 [ class "title" ]
-                    [ text "2048" ]
-                , div [ class "scores-container" ]
-                    [ div [ class "score-container" ]
-                        [ text "0" ]
-                    , div [ class "best-container" ]
-                        [ text "0" ]
-                    ]
-                ]
-            , div [ class "above-game" ]
-                [ p [ class "game-intro" ]
-                    [ text "Join the numbers and get to the "
-                    , strong []
-                        [ text "2048 tile!" ]
-                    ]
-                , a [ class "restart-button" ]
-                    [ text "New Game" ]
-                ]
+            [ gameHeader
+            , aboveGame
             , div [ class "game-container" ]
-                [ div [ class "game-message" ]
-                    [ p []
-                        []
-                    , div [ class "lower" ]
-                        [ a [ class "keep-playing-button" ]
-                            [ text "Keep going" ]
-                        , a [ class "retry-button" ]
-                            [ text "Try again" ]
-                        ]
-                    ]
-                , div [ class "grid-container" ]
-                    [ div [ class "grid-row" ]
-                        [ div [ class "grid-cell" ]
-                            []
-                        , div [ class "grid-cell" ]
-                            []
-                        , div [ class "grid-cell" ]
-                            []
-                        , div [ class "grid-cell" ]
-                            []
-                        ]
-                    , div [ class "grid-row" ]
-                        [ div [ class "grid-cell" ]
-                            []
-                        , div [ class "grid-cell" ]
-                            []
-                        , div [ class "grid-cell" ]
-                            []
-                        , div [ class "grid-cell" ]
-                            []
-                        ]
-                    , div [ class "grid-row" ]
-                        [ div [ class "grid-cell" ]
-                            []
-                        , div [ class "grid-cell" ]
-                            []
-                        , div [ class "grid-cell" ]
-                            []
-                        , div [ class "grid-cell" ]
-                            []
-                        ]
-                    , div [ class "grid-row" ]
-                        [ div [ class "grid-cell" ]
-                            []
-                        , div [ class "grid-cell" ]
-                            []
-                        , div [ class "grid-cell" ]
-                            []
-                        , div [ class "grid-cell" ]
-                            []
-                        ]
-                    ]
-                , div [ class "tile-container" ]
-                    []
+                [ gameMessage
+                , gridContainer
+                , tileContainer
                 ]
-            , p [ class "game-explanation" ]
-                [ strong [ class "important" ]
-                    [ text "How to play: " ]
-                , text "Use your "
-                , strong []
-                    [ text "arrow keys" ]
-                , text " to move the tiles. When two tiles with the same number touch, they "
-                , strong []
-                    [ text "merge into one!" ]
-                ]
-            , hr []
-                []
-            , p []
-                [ strong [ class "important" ]
-                    [ text "Note: " ]
-                , text "This is not the official version of 2048! It is an Elm implementation of Gabriele Cirulli's "
-                , a [ href "https://github.com/gabrielecirulli/2048" ]
-                    [ text "2048 game" ]
-                , text ". You can find the code for this Elm implementation here: "
-                , a [ href "https://github.com/stepheneb/elm-2048" ]
-                    [ text "github.com/stepheneb/elm-2048" ]
-                , text "."
-                ]
-            , hr []
-                []
-            , p []
-                [ text "Original 2048 created by "
-                , a [ href "http://gabrielecirulli.com", target "_blank" ]
-                    [ text "Gabriele Cirulli. " ]
-                , text "Based on "
-                , a [ href "https://itunes.apple.com/us/app/1024!/id823499224", target "_blank" ]
-                    [ text "1024 by Veewo Studio " ]
-                , text "and conceptually similar to "
-                , a [ href "http://asherv.com/threes/", target "_blank" ]
-                    [ text "Threes by Asher Vollmer." ]
-                ]
+            , gameExplanation
+            , divider
+            , gameNotes
+            , divider
+            , gameFooter
             ]
         ]
     }
+
+
+gameHeader : Html Msg
+gameHeader =
+    div [ class "heading" ]
+        [ h1 [ class "title" ]
+            [ text "2048" ]
+        , div [ class "scores-container" ]
+            [ div [ class "score-container" ]
+                [ text "0" ]
+            , div [ class "best-container" ]
+                [ text "0" ]
+            ]
+        ]
+
+
+aboveGame : Html Msg
+aboveGame =
+    div [ class "above-game" ]
+        [ p [ class "game-intro" ]
+            [ text "Join the numbers and get to the "
+            , strong []
+                [ text "2048 tile!" ]
+            ]
+        , a [ class "restart-button" ]
+            [ text "New Game" ]
+        ]
+
+
+gameMessage : Html Msg
+gameMessage =
+    div [ class "game-message" ]
+        [ p []
+            []
+        , div [ class "lower" ]
+            [ a [ class "keep-playing-button" ]
+                [ text "Keep going" ]
+            , a [ class "retry-button" ]
+                [ text "Try again" ]
+            ]
+        ]
+
+
+gridContainer : Html none
+gridContainer =
+    div [ class "grid-container" ]
+        [ div [ class "grid-row" ]
+            [ div [ class "grid-cell" ]
+                []
+            , div [ class "grid-cell" ]
+                []
+            , div [ class "grid-cell" ]
+                []
+            , div [ class "grid-cell" ]
+                []
+            ]
+        , div [ class "grid-row" ]
+            [ div [ class "grid-cell" ]
+                []
+            , div [ class "grid-cell" ]
+                []
+            , div [ class "grid-cell" ]
+                []
+            , div [ class "grid-cell" ]
+                []
+            ]
+        , div [ class "grid-row" ]
+            [ div [ class "grid-cell" ]
+                []
+            , div [ class "grid-cell" ]
+                []
+            , div [ class "grid-cell" ]
+                []
+            , div [ class "grid-cell" ]
+                []
+            ]
+        , div [ class "grid-row" ]
+            [ div [ class "grid-cell" ]
+                []
+            , div [ class "grid-cell" ]
+                []
+            , div [ class "grid-cell" ]
+                []
+            , div [ class "grid-cell" ]
+                []
+            ]
+        ]
+
+
+tileContainer : Html Msg
+tileContainer =
+    div [ class "tile-container" ]
+        []
+
+
+gameExplanation : Html none
+gameExplanation =
+    p [ class "game-explanation" ]
+        [ strong [ class "important" ]
+            [ text "How to play: " ]
+        , text "Use your "
+        , strong []
+            [ text "arrow keys" ]
+        , text " to move the tiles. When two tiles with the same number touch, they "
+        , strong []
+            [ text "merge into one!" ]
+        ]
+
+
+gameNotes : Html none
+gameNotes =
+    p []
+        [ strong [ class "important" ]
+            [ text "Note: " ]
+        , text "This is not the official version of 2048! It is an Elm implementation of Gabriele Cirulli's "
+        , a [ href "https://github.com/gabrielecirulli/2048" ]
+            [ text "2048 game" ]
+        , text ". You can find the code for this Elm implementation here: "
+        , a [ href "https://github.com/stepheneb/elm-2048" ]
+            [ text "github.com/stepheneb/elm-2048" ]
+        , text "."
+        ]
+
+
+gameFooter : Html none
+gameFooter =
+    p []
+        [ text "Original 2048 created by "
+        , a [ href "http://gabrielecirulli.com", target "_blank" ]
+            [ text "Gabriele Cirulli. " ]
+        , text "Based on "
+        , a [ href "https://itunes.apple.com/us/app/1024!/id823499224", target "_blank" ]
+            [ text "1024 by Veewo Studio " ]
+        , text "and conceptually similar to "
+        , a [ href "http://asherv.com/threes/", target "_blank" ]
+            [ text "Threes by Asher Vollmer." ]
+        ]
+
+
+divider : Html none
+divider =
+    hr []
+        []
 
 
 
